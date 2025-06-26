f43907dd5a8a0100aa6ef21e1b70bb091ebb70f8
Nishant <69218488+NishantCoder108@users.noreply.github.com>
2025-06-26 21:55:17 +0530

    Keygen complete

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # q3preq
+
+
+## Run The Program :
+- `yarn ts-node keygen.ts` 
---
+++ keygen.ts
@@ -0,0 +1,6 @@
+console.log("Hello");
+import { Keypair } from "@solana/web3.js";
+
+const kp = Keypair.generate();
+console.log(`Public Key: ${kp.publicKey.toBase58()}`);
+console.log(`[${kp.secretKey.toString()}]`);
