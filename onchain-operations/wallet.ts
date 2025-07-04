ac980e34077825f0fe4833d4389b7204ac29df88
Nishant <69218488+NishantCoder108@users.noreply.github.com>
2025-07-04 22:52:38 +0530

    encoded to bs58

--- onchain-operations/wallet.ts
+++ onchain-operations/wallet.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config'
 import { Connection, Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 import walletKeypair from "./onchain-wallet-key.json" with { type: "json" };
-
+import bs58 from "bs58";
 // *Generating Keypair:
 // const keypair = Keypair.generate();
 
@@ -32,12 +32,18 @@ const connection = new Connection(RPC_URL, "confirmed");
 
 
 // *Checking Balance:
-(async () => {
-  try {
-    console.log(`Public Key: ${keypair.publicKey.toBase58()}`);
-    const lam = await connection.getBalance(new PublicKey("ExUttmYkaNKjTPgg6yRkZdrdCH2VC1N5MDp7L424fCss"));
-    console.log(`Solana Balance : ${lam / LAMPORTS_PER_SOL} SOL`);
-  } catch (e) {
-    console.error(`Oops, something went wrong: ${e}`);
-  }
-})();
+// (async () => {
+//   try {
+//     console.log(`Public Key: ${keypair.publicKey.toBase58()}`);
+//     const lam = await connection.getBalance(new PublicKey("ExUttmYkaNKjTPgg6yRkZdrdCH2VC1N5MDp7L424fCss"));
+//     console.log(`Solana Balance : ${lam / LAMPORTS_PER_SOL} SOL`);
+//   } catch (e) {
+//     console.error(`Oops, something went wrong: ${e}`);
+//   }
+// })();
+
+
+// Walllet's Unitarray to base58 string
+let walletBase58 = Keypair.fromSecretKey(new Uint8Array(walletKeypair));
+console.log("Wallet Base58: ", walletBase58.publicKey.toBase58());
+console.log("Wallet Secrete Key: ", bs58.encode(walletBase58.secretKey));
