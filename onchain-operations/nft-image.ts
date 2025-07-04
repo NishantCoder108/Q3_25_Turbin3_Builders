58a1de593a6937bf7acccbfe03d206bff67ea4c8
Nishant <69218488+NishantCoder108@users.noreply.github.com>
2025-07-04 21:26:29 +0530

    Upload image to irys using metaplex umi and fix importing json file

---
+++ onchain-operations/nft-image.ts
@@ -0,0 +1,47 @@
+import 'dotenv/config'
+import wallet from './onchain-wallet-key.json' with { type: "json" };
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import {
+  createGenericFile,
+  createSignerFromKeypair,
+  signerIdentity,
+} from "@metaplex-foundation/umi";
+import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
+import { readFileSync } from 'fs';
+
+
+const RPC_URL = process.env.ALCHEMY_RPC_URL || "https://api.devnet.solana.com";
+console.log("RPC URL: ", RPC_URL);
+
+const umi = createUmi(RPC_URL, "confirmed");
+
+let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
+const signer = createSignerFromKeypair(umi, keypair);
+
+// umi.use(irysUploader());//for mainnet
+umi.use(irysUploader({ address: "https://devnet.irys.xyz/" }));
+umi.use(signerIdentity(signer));
+
+(async () => {
+  console.log("Uploading Image...");
+  try {
+    //1. Load image
+    const image = await readFileSync("./plane.png");
+    //2. Convert image to generic file.
+    let genericFile = createGenericFile(image, "mars-planet-travel.png", {
+      contentType: "image/png",
+    });
+    //3. Upload image
+    const [myUri] = await umi.uploader.upload([genericFile]);
+    console.log("image URI: ", myUri);
+  } catch (error) {
+    console.log("Oops.. Something went wrong", error);
+  }
+})();
+
+
+
+// // https://gateway.irys.xyz/A3N3Bo3Bhtg2dxVV1otmRdo63BMRnKs8wCMciq6t8Wnz
+// // https://gateway.irys.xyz/CSjVDFKuGC18jZi4ikQdXB9ew9Hyp7diK3x6H34Qg9K1
+
+//irys is fast compared to arweave
