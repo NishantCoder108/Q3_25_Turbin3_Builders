289c32c40f81ff245bde1a0ebcdc611526eef342
Nishant <69218488+NishantCoder108@users.noreply.github.com>
2025-07-09 23:58:39 +0530

    anchor vault setup

---
+++ anchor-vaults/programs/anchor-vaults/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("G4ezSKewUYAZ5zE8aquJhRMAvaBYJoHaEcAtUY4VigLt");
+
+#[program]
+pub mod anchor_vaults {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
